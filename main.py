08f53f60fe60840795179145a0d3e78b36fa2cdb
Johan-Hardick-git <firefly2520.1@gmail.com>
2026-09-18 20:11:02 +0200

    anchored the player to center of screen

--- main.py
+++ main.py
@@ -9,13 +9,21 @@ class Player:
         self.graphic = graphic
 
     def draw(self, screen):
-        pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, self.graphic, self.graphic))
+        pygame.draw.rect(screen, (255, 255, 255), (SCREEN_WIDTH/2, SCREEN_HEIGHT/2, self.graphic, self.graphic))
+
+    def relateX(self, x):
+        X = x + SCREEN_WIDTH/2 - self.x
+        return X
+    
+    def relateY(self, y):
+            Y = y + SCREEN_HEIGHT/2 - self.y
+            return Y
 
 border = {
     "XLeft": 0,
-    "XRight": 800,
+    "XRight": 3200,
     "YTop": 0,
-    "YBottom": 600,
+    "YBottom": 1600,
 }
 
 SCREEN_WIDTH = 800
@@ -28,7 +36,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Dungeons of Dagestan")
 
 clock = pygame.time.Clock()
-player = Player(0, 0, 10, 20)
+player = Player(15, border["YBottom"]/2, 10, 20)
 
 running = True
 
@@ -60,6 +68,13 @@ while running:
 
     screen.fill(BG_COLOUR)
     player.draw(screen)
+    pygame.draw.rect(screen, (255, 0, 0), (player.relateX(400), player.relateY(0), 10, 10))
+
+    pygame.draw.rect(screen, (255, 0, 0), (player.relateX(border["XLeft"]), player.relateY(0), 10, border["YBottom"]))
+    pygame.draw.rect(screen, (255, 0, 0), (player.relateX(border["XRight"] - 10), player.relateY(0), 10, border["YBottom"]))
+    pygame.draw.rect(screen, (255, 0, 0), (player.relateX(0), player.relateY(border["YTop"]), border["XRight"], 10))
+    pygame.draw.rect(screen, (255, 0, 0), (player.relateX(0), player.relateY(border["YBottom"] - 10), border["XRight"], 10))
+
     pygame.display.flip()
     clock.tick(60)
 
