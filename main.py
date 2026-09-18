4740e5177b893f52dc9af1e86ccfbd719b42b261
Johan-Hardick-git <firefly2520.1@gmail.com>
2026-09-18 21:14:31 +0200

    added small  health system

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import pygame
 
 
 class Player:
-    def __init__(self, x, y, speed, graphic):
+    def __init__(self, x, y, speed, health, graphic):
         self.x = x
         self.y = y
         self.speed = speed
+        self.health = health
         self.graphic = graphic
+        self.font = pygame.font.SysFont("arial", 32)
 
     def draw(self, screen):
         pygame.draw.rect(screen, (255, 255, 255), (SCREEN_WIDTH/2, SCREEN_HEIGHT/2, self.graphic, self.graphic))
+
+    def drawstats(self, screen):
+        text_surface = self.font.render(f"Health: {self.health}", True, WHITE)
+        text_rect = text_surface.get_rect(topleft=(20, 20))
+        screen.blit(text_surface, text_rect)
 
     def relateX(self, x):
         X = x + SCREEN_WIDTH/2 - self.x
@@ -18,6 +25,9 @@ class Player:
     def relateY(self, y):
             Y = y + SCREEN_HEIGHT/2 - self.y
             return Y
+
+    def damage(self, damn):
+        self.health -= damn
 
 border = {
     "XLeft": 0,
@@ -29,6 +39,7 @@ border = {
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 BG_COLOUR = (30, 30, 30)
+WHITE = (255, 255, 255)
 
 pygame.init()
 
@@ -36,7 +47,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Dungeons of Dagestan")
 
 clock = pygame.time.Clock()
-player = Player(15, border["YBottom"]/2, 10, 20)
+player = Player(15, border["YBottom"]/2, 10, 100, 20)
 
 running = True
 
@@ -67,13 +78,16 @@ while running:
         player.y = border["YBottom"] - player.graphic
 
     screen.fill(BG_COLOUR)
-    player.draw(screen)
+
     pygame.draw.rect(screen, (255, 0, 0), (player.relateX(400), player.relateY(0), 10, 10))
 
     pygame.draw.rect(screen, (255, 0, 0), (player.relateX(border["XLeft"]), player.relateY(0), 10, border["YBottom"]))
     pygame.draw.rect(screen, (255, 0, 0), (player.relateX(border["XRight"] - 10), player.relateY(0), 10, border["YBottom"]))
     pygame.draw.rect(screen, (255, 0, 0), (player.relateX(0), player.relateY(border["YTop"]), border["XRight"], 10))
     pygame.draw.rect(screen, (255, 0, 0), (player.relateX(0), player.relateY(border["YBottom"] - 10), border["XRight"], 10))
+
+    player.drawstats(screen)
+    player.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)
